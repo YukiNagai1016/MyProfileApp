c3b987fcb1cc48a3f479107ded107e53c7a2660d
優樹永井 <wadachi0505@gmail.com>
2018-11-08 17:09:50 +0900

    Initial Commit

---
+++ MyProfileApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProfileApp
+//
+//  Created by 優樹永井 on 2018/11/08.
+//  Copyright © 2018 優樹永井. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
